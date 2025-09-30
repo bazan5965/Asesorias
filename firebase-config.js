2eeff1ffad6ba52d4f01fc92bb54b16a9e8da620
bazan5965 <bazan5965@gmail.com>
2025-09-30 12:12:32 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.__firebase_config = {
-  apiKey: "AIzaSyXXXX",
-  authDomain: "mi-proyecto.firebaseapp.com",
-  projectId: "mi-proyecto",
-  storageBucket: "mi-proyecto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdefg"
+    apiKey: "AIzaSyXXXX",
+    authDomain: "mi-proyecto.firebaseapp.com",
+    projectId: "mi-proyecto",
+    storageBucket: "mi-proyecto.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:1234567890:web:abcdefg"
 };
